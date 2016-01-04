a0b7f337589a833e5ff912d2b2f55b3d56d5d01e
Adrian Matoga <epi@atari8.info>
2016-01-04 22:57:39 +0100

    Experimenting with various kinds of components.

---
+++ source/alsa/pcm.d
@@ -0,0 +1,103 @@
+///
+module alsa.pcm;
+
+pragma (lib, "asound");
+
+// Dummy struct, only pointers are used.
+struct snd_pcm_t
+{
+}
+
+enum snd_pcm_stream_t
+{
+	SND_PCM_STREAM_PLAYBACK = 0,
+	SND_PCM_STREAM_CAPTURE,
+	SND_PCM_STREAM_LAST = SND_PCM_STREAM_CAPTURE
+}
+
+enum snd_pcm_access_t
+{
+	SND_PCM_ACCESS_MMAP_INTERLEAVED = 0,
+	SND_PCM_ACCESS_MMAP_NONINTERLEAVED,
+	SND_PCM_ACCESS_MMAP_COMPLEX,
+	SND_PCM_ACCESS_RW_INTERLEAVED,
+	SND_PCM_ACCESS_RW_NONINTERLEAVED,
+	SND_PCM_ACCESS_LAST = SND_PCM_ACCESS_RW_NONINTERLEAVED
+}
+
+enum snd_pcm_format_t
+{
+	SND_PCM_FORMAT_UNKNOWN = -1,
+	SND_PCM_FORMAT_S8 = 0,
+	SND_PCM_FORMAT_U8,
+	SND_PCM_FORMAT_S16_LE,
+	SND_PCM_FORMAT_S16_BE,
+	SND_PCM_FORMAT_U16_LE,
+	SND_PCM_FORMAT_U16_BE,
+	SND_PCM_FORMAT_S24_LE,
+	SND_PCM_FORMAT_S24_BE,
+	SND_PCM_FORMAT_U24_LE,
+	SND_PCM_FORMAT_U24_BE,
+	SND_PCM_FORMAT_S32_LE,
+	SND_PCM_FORMAT_S32_BE,
+	SND_PCM_FORMAT_U32_LE,
+	SND_PCM_FORMAT_U32_BE,
+	SND_PCM_FORMAT_FLOAT_LE,
+	SND_PCM_FORMAT_FLOAT_BE,
+	SND_PCM_FORMAT_FLOAT64_LE,
+	SND_PCM_FORMAT_FLOAT64_BE,
+	SND_PCM_FORMAT_IEC958_SUBFRAME_LE,
+	SND_PCM_FORMAT_IEC958_SUBFRAME_BE,
+	SND_PCM_FORMAT_MU_LAW,
+	SND_PCM_FORMAT_A_LAW,
+	SND_PCM_FORMAT_IMA_ADPCM,
+	SND_PCM_FORMAT_MPEG,
+	SND_PCM_FORMAT_GSM,
+	SND_PCM_FORMAT_SPECIAL = 31,
+	SND_PCM_FORMAT_S24_3LE = 32,
+	SND_PCM_FORMAT_S24_3BE,
+	SND_PCM_FORMAT_U24_3LE,
+	SND_PCM_FORMAT_U24_3BE,
+	SND_PCM_FORMAT_S20_3LE,
+	SND_PCM_FORMAT_S20_3BE,
+	SND_PCM_FORMAT_U20_3LE,
+	SND_PCM_FORMAT_U20_3BE,
+	SND_PCM_FORMAT_S18_3LE,
+	SND_PCM_FORMAT_S18_3BE,
+	SND_PCM_FORMAT_U18_3LE,
+	SND_PCM_FORMAT_U18_3BE,
+	SND_PCM_FORMAT_LAST = SND_PCM_FORMAT_U18_3BE,
+}
+
+version (X86_64)
+{
+	alias long snd_pcm_sframes_t;
+	alias ulong snd_pcm_uframes_t;
+}
+else version (X86)
+{
+	alias int snd_pcm_sframes_t;
+	alias uint snd_pcm_uframes_t;
+}
+
+extern (C)
+int snd_pcm_open(snd_pcm_t **pcm, const char *name, snd_pcm_stream_t stream, int mode);
+
+extern (C)
+int snd_pcm_set_params(
+	snd_pcm_t *pcm,
+	snd_pcm_format_t format,
+	snd_pcm_access_t access,
+	uint channels,
+	uint rate,
+	int soft_resample,
+	uint latency);
+
+extern (C)
+snd_pcm_sframes_t snd_pcm_writei(snd_pcm_t *pcm, const void *buffer, snd_pcm_uframes_t size);
+
+extern (C)
+int snd_pcm_recover(snd_pcm_t *pcm, int err, int silent);
+
+extern (C)
+int snd_pcm_close(snd_pcm_t *pcm);
---
+++ source/samplerate.d
@@ -0,0 +1,92 @@
+///
+module samplerate;
+
+import core.stdc.config;
+
+import std.conv;
+import std.exception;
+
+struct SRC_STATE;
+
+struct SRC_DATA
+{
+	float*  data_in;
+	float*  data_out;
+
+	c_long  input_frames;
+	c_long  output_frames;
+	c_long  input_frames_used;
+	c_long  output_frames_gen;
+
+	int     end_of_input;
+
+	double  src_ratio;
+}
+
+struct SRC_CB_DATA
+{
+	long    frames;
+	float*  data_in;
+}
+
+alias extern(C) long function(void* cb_data, float** data) src_callback_t;
+
+extern (C)
+SRC_STATE* src_new(int converter_type, int channels, int *error);
+
+extern (C)
+SRC_STATE* src_callback_new(src_callback_t func, int converter_type, int channels,
+	int *error, void* cb_data);
+
+extern (C)
+SRC_STATE* src_delete(SRC_STATE* state);
+
+extern (C)
+int src_process(SRC_STATE* state, SRC_DATA* data);
+
+extern (C)
+c_long src_callback_read(SRC_STATE* state, double src_ratio, long frames, float* data);
+
+extern (C)
+int src_simple(SRC_DATA* data, int converter_type, int channels);
+
+extern (C)
+const(char)* src_get_name(int converter_type);
+extern (C)
+const(char)* src_get_description(int converter_type);
+extern (C)
+const(char)* src_get_version();
+
+extern (C)
+int src_set_ratio(SRC_STATE* state, double new_ratio);
+
+extern (C)
+int src_reset(SRC_STATE* state);
+
+extern (C)
+int src_is_valid_ratio(double ratio);
+
+extern (C)
+int src_error(SRC_STATE* state);
+
+extern (C)
+const(char)* src_strerror(int error);
+
+enum SRC_TYPE
+{
+	SRC_SINC_BEST_QUALITY		= 0,
+	SRC_SINC_MEDIUM_QUALITY		= 1,
+	SRC_SINC_FASTEST			= 2,
+	SRC_ZERO_ORDER_HOLD			= 3,
+	SRC_LINEAR					= 4,
+}
+
+extern (C)
+void src_short_to_float_array(const(short)* inp, float* outp, int len);
+extern (C)
+void src_float_to_short_array(const(float)* inp, short* outp, int len);
+
+extern (C)
+void src_int_to_float_array(const(int)* inp, float* outp, int len);
+extern (C)
+void src_float_to_int_array(const(float)* inp, int* outp, int len);
