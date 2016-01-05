a4b3ab8e7ec30b8021213b9c5eca3eed9d1c3197
Adrian Matoga <epi@atari8.info>
2016-01-05 22:05:59 +0100

    Playing with input stream and byLine.

--- source/app.d
+++ source/app.d
@@ -1,7 +1,7 @@
 ///
 module app;
 
-import std.stdio : writefln, stderr;
+import std.stdio : File, KeepTerminator, writeln, writefln, stderr;
 
 struct AlsaSink {
 	import alsa.pcm;
@@ -183,8 +183,6 @@ struct FromArray(T)
 }
 
 auto fromArray(T)(const(T[]) array) { return FromArray!T(array); }
-
-import std.stdio : File;
 
 struct FromFile
 {
@@ -391,8 +389,6 @@ auto compactPullBuffer(Source, T = SourceDataType!Source)(Source source)
 
 struct ToFile
 {
-	import std.stdio : File;
-
 	File file;
 
 	void push(const(ubyte)[] buf)
@@ -543,28 +539,134 @@ class PushToPullBuffer(Sink, ItsSink)
 	}
 }
 
+enum isSource(T) = true;
+enum isSink(T) = false;
+enum isBufferedPullSource(T) = true;
+
 // TODO: detect type of sink and itsSink
 auto pushToPullBuffer(Sink, ItsSink)(Sink sink, ItsSink itsSink)
 {
 	return new PushToPullBuffer!(Sink, ItsSink)(sink, itsSink);
 }
 
-int main(string[] args)
+struct InputStream(alias Comp, Type, T...)
+{
+	import std.typecons;
+	static if (T.length) {
+		Tuple!T tup;
+	}
+
+	static if (T.length) {
+		this(T args)
+		{
+			tup = tuple(args);
+		}
+	}
+
+	auto byLine(Terminator, Char = char)(KeepTerminator keepTerminator = KeepTerminator.no, Terminator terminator = '\x0a')
+	{
+		static struct Range
+		{
+			Type source;
+			bool keep;
+			Terminator term;
+			this(bool keep, Terminator term, T r)
+			{
+				static if (T.length > 0)
+					source = Type(r);
+				this.keep = keep;
+				this.term = term;
+			}
+			const(Char)[] line;
+
+			private void next()
+			{
+				size_t i = 0;
+				alias DT = typeof(source.peek(1)[0]);
+				const(DT)[] buf = source.peek(256);
+				if (buf.length == 0) {
+					line = null;
+					return;
+				}
+				while (buf[i] != term) {
+					i++;
+					if (i >= buf.length) {
+						buf = source.peek(buf.length * 2);
+						if (buf.length == i) {
+							line = buf[];
+							return;
+						}
+					}
+				}
+				line = buf[0 .. i + 1];
+			}
+			@property bool empty() { return line is null; }
+			void popFront()
+			{
+				source.consume(line.length);
+				next();
+			};
+			@property const(Char)[] front()
+			{
+				if (keep)
+					return line;
+				if (line.length > 0 && line[$ - 1] == term)
+					return line[0 .. $ - 1];
+				return line;
+			}
+		}
+		auto r = Range(keepTerminator == KeepTerminator.yes, terminator, tup.expand);
+		r.next();
+		return r;
+	}
+}
+
+
+import std.traits : isCallable;
+auto stream(alias Comp, T...)(T args)
+	if (isCallable!Comp || is(Comp) || isCallable!(Comp!T))
+{
+	alias Type = typeof(Comp(args));
+	static if (isCallable!Comp || is(Comp)) {
+		alias XComp = Comp;
+	} else {
+		alias XComp = Comp!T;
+	}
+	static if (isSource!Type && !isSink!Type)
+		return InputStream!(XComp, Type, T)(args);
+	else
+		static assert(false);
+	pragma(msg, Type.stringof);
+}
+
+void testByLine()
+{
+	import std.range : take, array;
+	auto testArray = "first line\nsecond line\nline without terminator";
+	/+
+	zassert(stream!fromArray(testArray).byLine(KeepTerminator.yes, 'e').array == [
+		"first line", "\nse", "cond line", "\nline", " without te", "rminator" ]);
+	zassert(stream!fromArray(testArray).byLine(KeepTerminator.no, 'e').array == [
+		"first lin", "\ns", "cond lin", "\nlin", " without t", "rminator" ]);
+	zassert(stream!fromArray(testArray).byLine!(char, char)(KeepTerminator.yes).array == [
+		"first line\n", "second line\n", "line without terminator" ]);
+	zassert(stream!fromArray(testArray).byLine!(char, char).array == [
+		"first line", "second line", "line without terminator" ]);
+	zassert(stream!fromArray(testArray).byLine(KeepTerminator.yes, 'z').array == [
+		"first line\nsecond line\nline without terminator" ]);+/
+	foreach (c; stream!fromArray(testArray).byLine(KeepTerminator.yes, '\n'))
+		c.writeln();
+}
+
+int testMad()
 {
 	import core.sys.posix.sys.stat;
 	import core.sys.posix.sys.mman;
 	import core.sys.posix.unistd;
-	import std.stdio : stdout;
-
 //	import etc.linux.memoryerror;
 //	registerMemoryErrorHandler();
-
-	import std.stdio : stdin;
 	stat_t st;
 	void *fdm;
-
-	if (args.length != 1)
-		return 1;
 
 //	if (fstat(STDIN_FILENO, &st) == -1 || st.st_size == 0)
 //		return 2;
@@ -595,5 +697,31 @@ int main(string[] args)
 //	if (munmap(fdm, st.st_size) == -1)
 //		return 4;
 
-  return 0;
+	return 0;
+}
+
+int main(string[] args)
+{
+	testByLine();
+
+	auto stream1 = stream!curlReader("file:///media/epi/Passport/music/chips_n_games/dubmood_2/sc/toffelskater_2007.128.mp3");
+	auto stream2 = stream!madDecoder();
+	auto stream3 = stream!MadDecoder;
+
+
+	/+
+	stream!PosixFile("some_movie.avi")
+		.pipe!AviDemux()
+		.tee!("audio", "video")(
+			stream!MadDecoder.pipe!AlsaSink(2, 44100, 16),
+			stream!MpegDecoder.pipe!SDLVideo())
+		.run();
+
+	auto vs = stream!Capture("/dev/video0");
+	auto as = stream!AlsaRecord();
+	merge!("video", "audio")(vs, as).pipe!AviMux.pipe!PosixFile("recorded_movie.avi");
+
+	stream!Capture("/dev/video0").merge!AlsaRecord.pipe!AviMux.pipe!PosixFile("recorded_movie.avi");
++/
+	return 0;
 }
