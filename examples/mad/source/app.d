eaf0ead0ef9b19f57eb70a65acbd7a2436088d85
Adrian Matoga <epi@atari8.info>
2016-02-28 12:22:27 +0100

    [examples/mad] Use pull-style file input and flod.etc.mad.decodeMp3.

--- examples/mad/source/app.d
+++ examples/mad/source/app.d
@@ -2,16 +2,18 @@ import std.exception : enforce;
 
 import flod.etc.alsa;
 import flod.etc.mad;
-import flod.file : MmappedFile;
+import flod.file : fromFile;
 import flod.pipeline;
 import flod.traits;
+import std.stdio;
 
 int main(string[] args)
 {
-	enforce(args.length > 1);
+	enforce(args.length > 1, "Specify a file to play");
 	foreach (fn; args[1 .. $]) {
-		pipe!MmappedFile(fn)
-			.pipe!MadDecoder
+		auto f = File(fn, "rb");
+		fromFile(File(fn))
+			.decodeMp3
 			.pipe!AlsaPcm(2, 44100, 16)
 			.run();
 	}
