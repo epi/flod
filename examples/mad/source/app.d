ae4b8b8f3c06675a085672c1c8f42c90508d685b
Adrian Matoga <epi@atari8.info>
2016-03-03 21:29:02 +0100

    [examples/mad] Use flod-curl to play files as they're downloaded.

--- examples/mad/source/app.d
+++ examples/mad/source/app.d
@@ -2,6 +2,7 @@ import std.exception : enforce;
 
 import flod.etc.alsa;
 import flod.etc.mad;
+import flod.etc.curl;
 import flod.file : fromFile;
 import flod.pipeline;
 import flod.traits;
@@ -11,8 +12,7 @@ int main(string[] args)
 {
 	enforce(args.length > 1, "Specify a file to play");
 	foreach (fn; args[1 .. $]) {
-		auto f = File(fn, "rb");
-		fromFile(File(fn))
+		download(fn)
 			.decodeMp3
 			.pipe!AlsaPcm(2, 44100, 16)
 			.run();
