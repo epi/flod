d54b14dc01623822c3fb3173ad2fbaf8eb23b623
Adrian Matoga <epi@atari8.info>
2016-03-13 18:51:26 +0100

    [examples/mad] Use file:// as default protocol prefix.

--- examples/mad/source/app.d
+++ examples/mad/source/app.d
@@ -1,22 +1,17 @@
 import std.exception : enforce;
+import std.regex : match;
 
 import flod.etc.alsa;
 import flod.etc.mad;
 import flod.etc.curl;
 import flod.pipeline;
-import flod.adapter;
-import flod.traits;
-import std.stdio;
 
 int main(string[] args)
 {
 	enforce(args.length > 1, "Specify a file to play");
 	foreach (fn; args[1 .. $]) {
-		download(fn)
-			.pushPull
-			.pullPeek
-			.decodeMp3
-			.pipe!AlsaPcm(2, 44100, 16);
+		auto url = args[1].match(`^[a-z]+://`).empty ? "file://" ~ fn : fn;
+		download(url).decodeMp3.pipe!AlsaPcm(2, 44100, 16);
 	}
 	return 0;
 }
